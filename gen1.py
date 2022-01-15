efbc3c15fe5edb7b2f354570c096e38a788c8e74
atripptwice <atripp@protonmail.com>
2022-01-15 11:19:51 -0500

    Updated gen1.py to show bar graph

--- gen1.py
+++ gen1.py
@@ -1,5 +1,6 @@
 #csv from https://gist.github.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6
 import pandas as pd
+import matplotlib.pyplot as plt
 #read csv into datafrom
 df = pd.read_csv("pokemon.csv")
 #create a boolean mask True if pokemon is not mega
@@ -8,8 +9,7 @@ mask =  ~df['Name'].str.contains('Mega ')
 df = df[mask]
 #slice to only be GEN 1
 gen1 = df.iloc[0:151]
-print(gen1)
-
+gen1.groupby(['Type 1', 'Type 2']).size().plot(kind='bar')
 
 
 
