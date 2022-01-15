bc0ad4851cfb3f4f2ebb2771b449fff3b3dc569d
atripptwice <atripp@protonmail.com>
2022-01-15 07:03:08 -0500

    Added csv and working gen1 file

---
+++ gen1.py
@@ -0,0 +1,15 @@
+#csv from https://gist.github.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6
+import pandas as pd
+#read csv into datafrom
+df = pd.read_csv("pokemon.csv")
+#create a boolean mask True if pokemon is not mega
+mask =  ~df['Name'].str.contains('Mega ')
+#remove mega evolutions
+df = df[mask]
+#slice to only be GEN 1
+gen1 = df.iloc[0:151]
+print(gen1)
+
+
+
+
